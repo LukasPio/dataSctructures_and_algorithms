4cd66aeaa7688c8604ba05a6fbbdc8d39468ef49
lucas <lucaspio.galvao@gmail.com>
2023-11-25 22:12:30 -0300

    algoritimo de filtro em array

---
+++ src/NegativePositiveNulPercentageInArray.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class NegativePositiveNulPercentageInArray {
+    static void plusMinus (List<Integer> array) {
+        double negativePercent = 0, positivePercent = 0, nullPercent = 0;
+
+        for (int number: array) {
+            if (number > 0) {
+                positivePercent++;
+            }
+            else if (number < 0) {
+                negativePercent++;
+            }
+            else {
+                nullPercent++;
+            }
+        }
+        System.out.println(positivePercent / array.size());
+        System.out.println(negativePercent / array.size());
+        System.out.println(nullPercent / array.size());
+    }
+    public static void main(String[] args) {
+        List<Integer> listOfNumbers = List.of(
+                1, 5, 2, 8, -2, -7, -62, 0, 0, -5
+        );
+
+        plusMinus(listOfNumbers);
+    }
+}
