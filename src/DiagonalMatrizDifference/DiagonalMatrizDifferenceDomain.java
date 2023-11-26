ef76072d958facff8d30719f5b5ce7c74f8cea55
lucas <lucaspio.galvao@gmail.com>
2023-11-25 21:52:57 -0300

    feat: aceitar List como param

--- src/DiagonalMatrizDifference/DiagonalMatrizDifferenceDomain.java
+++ src/DiagonalMatrizDifference/DiagonalMatrizDifferenceDomain.java
@@ -4,27 +4,28 @@ import java.util.List;
 
 public class DiagonalMatrizDifferenceDomain {
     public static int calculateDiagnoalDifference(int[][] matriz) {
-        int matrizSize = matriz.length;
         int primaryDiagonal = 0, secondaryDiagonal = 0;
-        int column = 0;
-        for (int i = 1; i < matrizSize; i++) {
-            primaryDiagonal += matriz[i][column];
-            secondaryDiagonal += matriz[i][matriz[0][0] - 1 - column];
-            column++;
+
+        for (int i = 0; i < matriz.length; i++) {
+            primaryDiagonal += matriz[i][i];
+            secondaryDiagonal += matriz[i][matriz.length - 1 - i];
         }
 
         return Math.abs(primaryDiagonal - secondaryDiagonal);
     }
 
     public static int calculateDiagnoalDifference(List<List<Integer>> matriz) {
-        int primaryDiagonal = 0;
-        int secondaryDiagonal = 0;
+        int primaryDiagonal = 0, secondaryDiagonal = 0;
+
+//        {1, 2, 3},
+//        {4, 5, 6},
+//        {7, 8, 9}
 
         for (int i = 0; i < matriz.size(); i++) {
-            primaryDiagonal = matriz.get(i).get(i);
-            secondaryDiagonal = matriz.get(i).get(matriz.size() - 1 - i);
+            primaryDiagonal += matriz.get(i).get(i);
+            secondaryDiagonal += matriz.get(i).get(matriz.size() - 1 - i);
         }
 
-        return primaryDiagonal - secondaryDiagonal;
+        return Math.abs(primaryDiagonal - secondaryDiagonal);
     }
 }
--- src/DiagonalMatrizDifference/DiagonalMatrizDifferenceTest.java
+++ src/DiagonalMatrizDifference/DiagonalMatrizDifferenceTest.java
@@ -1,7 +1,26 @@
 package DiagonalMatrizDifference;
 
+import java.util.List;
+
 public class DiagonalMatrizDifferenceTest {
     public static void main(String[] args) {
 
+        List<List<Integer>> matrizList = List.of(
+                List.of(2, 2, 3),
+                List.of(4, 5, 6),
+                List.of(7, 8, 9)
+        );
+
+        int[][] matrizArray = {
+                {2, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println(DiagonalMatrizDifferenceDomain.
+                calculateDiagnoalDifference(matrizList));
+
+        System.out.println(DiagonalMatrizDifferenceDomain.
+                calculateDiagnoalDifference(matrizArray));
     }
 }
