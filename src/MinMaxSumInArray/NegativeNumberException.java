29770f82e20c317332d0ba74d7c3ee4c1be3f262
lucas <lucaspio.galvao@gmail.com>
2023-11-27 23:08:53 -0300

    Algoritimo de MinMaxSum em um array

--- src/MinMaxSumInArray/NegativeNumberException.java
+++
@@ -1,7 +0,0 @@
-package MinMaxSumInArray;
-
-public class NegativeNumberException extends Exception {
-    public NegativeNumberException () {
-        super("Negative numbers aren't permited");
-    }
-}
