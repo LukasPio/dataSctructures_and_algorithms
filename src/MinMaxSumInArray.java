dfa0a6e525481b55e3bb3cc64596778bfe8e2eb4
lucas <lucaspio.galvao@gmail.com>
2023-11-27 23:12:29 -0300

    Optimizações no algoritimo

--- src/MinMaxSumInArray.java
+++ src/MinMaxSumInArray.java
@@ -2,36 +2,42 @@ import java.util.List;
 
 public class MinMaxSumInArray {
     public static void main(String[] args) {
+
         List<Integer> fivePositiveIntegerNumbers = List.of(
                 7, 14, 3, 9, 22
         );
 
         int higherNumber = fivePositiveIntegerNumbers.get(0),
                 secondaryHigherNumber = fivePositiveIntegerNumbers.get(1);
+
         int smallerNumber = fivePositiveIntegerNumbers.get(0),
                 secondarySmallerNumber = fivePositiveIntegerNumbers.get(1);
 
-        for (int i = 0; i < fivePositiveIntegerNumbers.size(); i++) {
-            int aNumber = fivePositiveIntegerNumbers.get(i);
+        for (int aNumber : fivePositiveIntegerNumbers) {
+
             if (higherNumber < aNumber) {
                 higherNumber = aNumber;
             }
+
             if (smallerNumber > aNumber) {
                 smallerNumber = aNumber;
             }
         }
 
-        for (int i = 0; i < fivePositiveIntegerNumbers.size(); i++) {
-            int aNumber = fivePositiveIntegerNumbers.get(i);
+        for (int aNumber : fivePositiveIntegerNumbers) {
+
             if (secondaryHigherNumber < aNumber && aNumber != higherNumber) {
                 secondaryHigherNumber = aNumber;
             }
+
             if (secondarySmallerNumber > aNumber && aNumber != smallerNumber) {
                 secondarySmallerNumber = aNumber;
             }
         }
+
         int biggerSum = higherNumber + secondaryHigherNumber;
         int smallerSum = smallerNumber + secondarySmallerNumber;
+
         System.out.println(biggerSum);
         System.out.println(smallerSum);
     }
