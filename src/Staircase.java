c0d5d49f0936c8f6b5452a83bc3b89fb9901cd4e
lucas <lucaspio.galvao@gmail.com>
2023-11-27 22:48:10 -0300

    Algoritimo de Staircase

---
+++ src/Staircase.java
@@ -0,0 +1,14 @@
+public class Staircase {
+    public static void main(String[] args) {
+        int stairHeight = 6;
+        StringBuilder spaces = new StringBuilder();
+        spaces.append(" ".repeat(stairHeight - 1));
+        String aLine = "#";
+
+        for (int i = 0; i < stairHeight; i++) {
+            System.out.println(spaces + aLine);
+            aLine += "#";
+            spaces.delete(0, 1);
+        }
+    }
+}
