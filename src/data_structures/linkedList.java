83ea8a5e7515cb91b810d14f5132ae1ae58720ab
lucas <lucaspio.galvao@gmail.com>
2024-01-30 08:16:32 -0300

    Linked list

---
+++ src/data_structures/linkedList.java
@@ -0,0 +1,10 @@
+package data_structures;
+
+import java.util.LinkedList;
+
+public class linkedList {
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+
+    }
+}
