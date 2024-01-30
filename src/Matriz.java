43626544f4b02378368a125b87c786af5f524c94
lucas <lucaspio.galvao@gmail.com>
2024-01-30 08:17:31 -0300

    Matriz

--- src/Matriz.java
+++ src/Matriz.java
@@ -5,17 +5,17 @@ public class Matriz {
 
         final int totalOfRow = 3;
         final int totalOfColumn = 5;
-        int[][] matriz = new int[totalOfRow][totalOfColumn];
+        int[][] matrix = new int[totalOfRow][totalOfColumn];
 
         for (int row = 0; row < totalOfRow; row++) {
             for (int column = 0; column < totalOfColumn; column++) {
-                matriz[row][column] = new SecureRandom().nextInt(100);
+                matrix[row][column] = new SecureRandom().nextInt(100);
             }
         }
 
         for (int row = 0; row < totalOfRow; row++) {
             for (int column = 0; column < totalOfColumn; column++) {
-                System.out.printf("%d " ,matriz[row][column]);
+                System.out.printf("%d " ,matrix[row][column]);
             }
             System.out.printf("%n");
         }
